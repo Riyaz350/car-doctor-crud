6568c9fc3e45145e55b9db0a48f14d4f86b31b19
riyaz <riyaz3582@gmail.com>
2023-11-02 08:31:25 +0600

    first commit

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,20 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyANiW8cZ_qo3CFFBD46K5cNPNnOdhrlles",
+  authDomain: "car-doctor-crud-38f97.firebaseapp.com",
+  projectId: "car-doctor-crud-38f97",
+  storageBucket: "car-doctor-crud-38f97.appspot.com",
+  messagingSenderId: "988286959315",
+  appId: "1:988286959315:web:1e2ce61d246813dfc39e26",
+  measurementId: "G-ZY4LJCYM34"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
